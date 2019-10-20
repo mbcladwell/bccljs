ff1bccc92fa8e96ae74624194395b99f88d9932b
mbcladwell <mbcladwell@stihie.net>
2019-10-20 08:49:47 -0400

    mysql loaded

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]]
+                 [org.clojure/clojurescript "1.10.339"]
+                  [org.clojure/java.jdbc "0.7.5"]
+                 [mysql/mysql-connector-java "8.0.11"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
