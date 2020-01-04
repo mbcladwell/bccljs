a03a74cd3b0c723960f6addc130dea5947cc1dde
mbcladwell <mbcladwell@stihie.net>
2020-01-04 10:28:49 -0500

    sat morn fresh start

---
+++ bccljs.core/resources/public/cljs-out/dev/bccljs/core_test.js
@@ -0,0 +1,43 @@
+// Compiled by ClojureScript 1.10.520 {}
+goog.provide('bccljs.core_test');
+goog.require('cljs.core');
+goog.require('cljs.test');
+goog.require('bccljs.core');
+bccljs.core_test.multiply_test = (function bccljs$core_test$multiply_test(){
+return cljs.test.test_var.call(null,bccljs.core_test.multiply_test.cljs$lang$var);
+});
+bccljs.core_test.multiply_test.cljs$lang$test = (function (){
+try{var values__9903__auto__ = (new cljs.core.List(null,((1) * (2)),(new cljs.core.List(null,bccljs.core.multiply.call(null,(1),(2)),null,(1),null)),(2),null));
+var result__9904__auto__ = cljs.core.apply.call(null,cljs.core._EQ_,values__9903__auto__);
+if(cljs.core.truth_(result__9904__auto__)){
+cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"pass","pass",1574159993),new cljs.core.Keyword(null,"expected","expected",1583670997),cljs.core.list(new cljs.core.Symbol(null,"=","=",-1501502141,null),cljs.core.list(new cljs.core.Symbol(null,"*","*",345799209,null),(1),(2)),cljs.core.list(new cljs.core.Symbol(null,"multiply","multiply",603624479,null),(1),(2))),new cljs.core.Keyword(null,"actual","actual",107306363),cljs.core.cons.call(null,cljs.core._EQ_,values__9903__auto__),new cljs.core.Keyword(null,"message","message",-406056002),null], null));
+} else {
+cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"fail","fail",1706214930),new cljs.core.Keyword(null,"expected","expected",1583670997),cljs.core.list(new cljs.core.Symbol(null,"=","=",-1501502141,null),cljs.core.list(new cljs.core.Symbol(null,"*","*",345799209,null),(1),(2)),cljs.core.list(new cljs.core.Symbol(null,"multiply","multiply",603624479,null),(1),(2))),new cljs.core.Keyword(null,"actual","actual",107306363),(new cljs.core.List(null,new cljs.core.Symbol(null,"not","not",1044554643,null),(new cljs.core.List(null,cljs.core.cons.call(null,new cljs.core.Symbol(null,"=","=",-1501502141,null),values__9903__auto__),null,(1),null)),(2),null)),new cljs.core.Keyword(null,"message","message",-406056002),null], null));
+}
+
+return result__9904__auto__;
+}catch (e10799){var t__9948__auto__ = e10799;
+return cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"error","error",-978969032),new cljs.core.Keyword(null,"expected","expected",1583670997),cljs.core.list(new cljs.core.Symbol(null,"=","=",-1501502141,null),cljs.core.list(new cljs.core.Symbol(null,"*","*",345799209,null),(1),(2)),cljs.core.list(new cljs.core.Symbol(null,"multiply","multiply",603624479,null),(1),(2))),new cljs.core.Keyword(null,"actual","actual",107306363),t__9948__auto__,new cljs.core.Keyword(null,"message","message",-406056002),null], null));
+}});
+
+bccljs.core_test.multiply_test.cljs$lang$var = new cljs.core.Var(function(){return bccljs.core_test.multiply_test;},new cljs.core.Symbol("bccljs.core-test","multiply-test","bccljs.core-test/multiply-test",1314842345,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"bccljs.core-test","bccljs.core-test",-24277023,null),new cljs.core.Symbol(null,"multiply-test","multiply-test",373518316,null),"/home/mbc/projects/bccljs/bccljs.core/test/bccljs/core_test.cljs",23,1,6,6,cljs.core.List.EMPTY,null,(cljs.core.truth_(bccljs.core_test.multiply_test)?bccljs.core_test.multiply_test.cljs$lang$test:null)]));
+bccljs.core_test.multiply_test_2 = (function bccljs$core_test$multiply_test_2(){
+return cljs.test.test_var.call(null,bccljs.core_test.multiply_test_2.cljs$lang$var);
+});
+bccljs.core_test.multiply_test_2.cljs$lang$test = (function (){
+try{var values__9903__auto__ = (new cljs.core.List(null,((75) * (10)),(new cljs.core.List(null,bccljs.core.multiply.call(null,(10),(75)),null,(1),null)),(2),null));
+var result__9904__auto__ = cljs.core.apply.call(null,cljs.core._EQ_,values__9903__auto__);
+if(cljs.core.truth_(result__9904__auto__)){
+cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"pass","pass",1574159993),new cljs.core.Keyword(null,"expected","expected",1583670997),cljs.core.list(new cljs.core.Symbol(null,"=","=",-1501502141,null),cljs.core.list(new cljs.core.Symbol(null,"*","*",345799209,null),(75),(10)),cljs.core.list(new cljs.core.Symbol(null,"multiply","multiply",603624479,null),(10),(75))),new cljs.core.Keyword(null,"actual","actual",107306363),cljs.core.cons.call(null,cljs.core._EQ_,values__9903__auto__),new cljs.core.Keyword(null,"message","message",-406056002),null], null));
+} else {
+cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"fail","fail",1706214930),new cljs.core.Keyword(null,"expected","expected",1583670997),cljs.core.list(new cljs.core.Symbol(null,"=","=",-1501502141,null),cljs.core.list(new cljs.core.Symbol(null,"*","*",345799209,null),(75),(10)),cljs.core.list(new cljs.core.Symbol(null,"multiply","multiply",603624479,null),(10),(75))),new cljs.core.Keyword(null,"actual","actual",107306363),(new cljs.core.List(null,new cljs.core.Symbol(null,"not","not",1044554643,null),(new cljs.core.List(null,cljs.core.cons.call(null,new cljs.core.Symbol(null,"=","=",-1501502141,null),values__9903__auto__),null,(1),null)),(2),null)),new cljs.core.Keyword(null,"message","message",-406056002),null], null));
+}
+
+return result__9904__auto__;
+}catch (e10800){var t__9948__auto__ = e10800;
+return cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"error","error",-978969032),new cljs.core.Keyword(null,"expected","expected",1583670997),cljs.core.list(new cljs.core.Symbol(null,"=","=",-1501502141,null),cljs.core.list(new cljs.core.Symbol(null,"*","*",345799209,null),(75),(10)),cljs.core.list(new cljs.core.Symbol(null,"multiply","multiply",603624479,null),(10),(75))),new cljs.core.Keyword(null,"actual","actual",107306363),t__9948__auto__,new cljs.core.Keyword(null,"message","message",-406056002),null], null));
+}});
+
+bccljs.core_test.multiply_test_2.cljs$lang$var = new cljs.core.Var(function(){return bccljs.core_test.multiply_test_2;},new cljs.core.Symbol("bccljs.core-test","multiply-test-2","bccljs.core-test/multiply-test-2",-956365945,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"bccljs.core-test","bccljs.core-test",-24277023,null),new cljs.core.Symbol(null,"multiply-test-2","multiply-test-2",2128120474,null),"/home/mbc/projects/bccljs/bccljs.core/test/bccljs/core_test.cljs",25,1,9,9,cljs.core.List.EMPTY,null,(cljs.core.truth_(bccljs.core_test.multiply_test_2)?bccljs.core_test.multiply_test_2.cljs$lang$test:null)]));
+
+//# sourceMappingURL=core_test.js.map
---
+++ bccljs.core/resources/public/cljs-out/dev/figwheel/main/generated/dev_auto_test_runner.js
@@ -0,0 +1,58 @@
+// Compiled by ClojureScript 1.10.520 {}
+goog.provide('figwheel.main.generated.dev_auto_test_runner');
+goog.require('cljs.core');
+goog.require('cljs.test');
+goog.require('cljs_test_display.core');
+goog.require('bccljs.core_test');
+cljs.test.run_block.call(null,(function (){var env11014 = cljs_test_display.core.init_BANG_.call(null,"app-auto-testing");
+var summary11015 = cljs.core.volatile_BANG_.call(null,new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"summary","summary",380847952),new cljs.core.Keyword(null,"fail","fail",1706214930),(0),new cljs.core.Keyword(null,"error","error",-978969032),(0),new cljs.core.Keyword(null,"pass","pass",1574159993),(0),new cljs.core.Keyword(null,"test","test",577538877),(0)], null));
+return cljs.core.concat.call(null,cljs.core.concat.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env11014,summary11015){
+return (function (){
+cljs.test.set_env_BANG_.call(null,env11014);
+
+cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Symbol(null,"bccljs.core-test","bccljs.core-test",-24277023,null),new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"begin-test-ns","begin-test-ns",-1701237033)], null));
+
+return cljs.test.block.call(null,(function (){var env__10016__auto__ = cljs.test.get_current_env.call(null);
+return cljs.core.concat.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__10016__auto__,env11014,summary11015){
+return (function (){
+if((env__10016__auto__ == null)){
+cljs.test.set_env_BANG_.call(null,cljs.test.empty_env.call(null));
+} else {
+}
+
+
+return null;
+});})(env__10016__auto__,env11014,summary11015))
+], null),cljs.test.test_vars_block.call(null,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Var(function(){return bccljs.core_test.multiply_test;},new cljs.core.Symbol("bccljs.core-test","multiply-test","bccljs.core-test/multiply-test",1314842345,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"bccljs.core-test","bccljs.core-test",-24277023,null),new cljs.core.Symbol(null,"multiply-test","multiply-test",373518316,null),"/home/mbc/projects/bccljs/bccljs.core/test/bccljs/core_test.cljs",23,1,6,6,cljs.core.List.EMPTY,null,(cljs.core.truth_(bccljs.core_test.multiply_test)?bccljs.core_test.multiply_test.cljs$lang$test:null)])),new cljs.core.Var(function(){return bccljs.core_test.multiply_test_2;},new cljs.core.Symbol("bccljs.core-test","multiply-test-2","bccljs.core-test/multiply-test-2",-956365945,null),cljs.core.PersistentHashMap.fromArrays([new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Keyword(null,"name","name",1843675177),new cljs.core.Keyword(null,"file","file",-1269645878),new cljs.core.Keyword(null,"end-column","end-column",1425389514),new cljs.core.Keyword(null,"column","column",2078222095),new cljs.core.Keyword(null,"line","line",212345235),new cljs.core.Keyword(null,"end-line","end-line",1837326455),new cljs.core.Keyword(null,"arglists","arglists",1661989754),new cljs.core.Keyword(null,"doc","doc",1913296891),new cljs.core.Keyword(null,"test","test",577538877)],[new cljs.core.Symbol(null,"bccljs.core-test","bccljs.core-test",-24277023,null),new cljs.core.Symbol(null,"multiply-test-2","multiply-test-2",2128120474,null),"/home/mbc/projects/bccljs/bccljs.core/test/bccljs/core_test.cljs",25,1,9,9,cljs.core.List.EMPTY,null,(cljs.core.truth_(bccljs.core_test.multiply_test_2)?bccljs.core_test.multiply_test_2.cljs$lang$test:null)]))], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env__10016__auto__,env11014,summary11015){
+return (function (){
+if((env__10016__auto__ == null)){
+return cljs.test.clear_env_BANG_.call(null);
+} else {
+return null;
+}
+});})(env__10016__auto__,env11014,summary11015))
+], null));
+})());
+});})(env11014,summary11015))
+,((function (env11014,summary11015){
+return (function (){
+return cljs.test.do_report.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"ns","ns",441598760),new cljs.core.Symbol(null,"bccljs.core-test","bccljs.core-test",-24277023,null),new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"end-test-ns","end-test-ns",1620675645)], null));
+});})(env11014,summary11015))
+], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env11014,summary11015){
+return (function (){
+return cljs.core._vreset_BANG_.call(null,summary11015,cljs.core.partial.call(null,cljs.core.merge_with,cljs.core._PLUS_).call(null,cljs.core._deref.call(null,summary11015),new cljs.core.Keyword(null,"report-counters","report-counters",-1702609242).cljs$core$IFn$_invoke$arity$1(cljs.test.get_and_clear_env_BANG_.call(null))));
+});})(env11014,summary11015))
+], null)),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [((function (env11014,summary11015){
+return (function (){
+cljs.test.set_env_BANG_.call(null,env11014);
+
+cljs.test.do_report.call(null,cljs.core.deref.call(null,summary11015));
+
+cljs.test.report.call(null,cljs.core.assoc.call(null,cljs.core.deref.call(null,summary11015),new cljs.core.Keyword(null,"type","type",1174270348),new cljs.core.Keyword(null,"end-run-tests","end-run-tests",267300563)));
+
+return cljs.test.clear_env_BANG_.call(null);
+});})(env11014,summary11015))
+], null));
+})());
+
+//# sourceMappingURL=dev_auto_test_runner.js.map
